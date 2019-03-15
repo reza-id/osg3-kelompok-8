b9a2eb15bc0581c6814e93b045ad20929ed8fec2
wiliarko <wiliarko_156@yahoo.co.id>
2019-03-16 01:10:58 +0700

    repository

---
+++ app/src/main/java/id/eudeka/delapan/view/ListFragment.java
@@ -0,0 +1,21 @@
+package id.eudeka.delapan.view;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.eudeka.delapan.R;
+
+public class ListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_incident, container, false);
+        return view;
+    }
+}
